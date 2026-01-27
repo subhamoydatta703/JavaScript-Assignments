ae479dd4cb4ca6ed09ddf5c21275bc66e6313805
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-27 20:55:38 +0530

    "File cleaning"

--- 01-async-js/medium/1-file-cleaner.js
+++ 01-async-js/medium/1-file-cleaner.js
@@ -1,6 +1,25 @@
 // ## File cleaner
 // Read a file, remove all the extra spaces and write it back to the same file.
 
+const fs = require('fs').promises;
+async function fileReadAndWrite() {
+
+    try {
+        const data = await fs.readFile('sample_file.txt', 'utf8');
+        console.log("File content: ",data);
+        const cleanData = data.replace(/\s+/g, ' ').trim();
+        console.log("Cleaned data: ", cleanData);
+
+         await fs.writeFile('sample_file.txt',cleanData,'utf8');
+        
+
+    } catch (error) {
+console.error("Error: ",error);
+
+    }
+}
+fileReadAndWrite();
+
 // For example, if the file input was
 // ```
 // hello     world    my    name   is       raman
