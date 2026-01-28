636ee10f5e350a7acd28683ea3565b16e7b77986
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-28 11:46:52 +0530

    hard(promises) questions number 3 solution

--- 01-async-js/hard (promises)/3-promise-all.js
+++ 01-async-js/hard (promises)/3-promise-all.js
@@ -6,18 +6,52 @@
 
 function wait1(t) {
 
+    let p1 = new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("First promise");
+            resolve("Wait1")
+        }, t * 1000);
+
+
+    },)
+    return p1
 }
 
 function wait2(t) {
+    let p2 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Wait2")
+            console.log("Second promise");
+        }, t * 1000);
 
+    })
+    return p2
 }
 
 function wait3(t) {
+    let p3 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Wait3")
+            console.log("Third promise");
+        }, t * 1000);
 
+    })
+    return p3
 }
 
 function calculateTime(t1, t2, t3) {
-
+    let start = Date.now()
+   return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then((values) => {
+        console.log(values);
+        let end = Date.now();
+        return end - start;
+    });
+    // console.log("Total time: ", ((t1+t2+t3)/1000), "ms");
 }
+
+calculateTime(2, 3, 5).then((x) => {
+    console.log(`Time: ${x} ms`);
+
+})
 
 module.exports = calculateTime;
